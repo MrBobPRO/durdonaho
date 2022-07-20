1dc104af898576d57d348bba849185d9db8cc444
MrBobPRO <bobnuridinov@gmail.com>
2022-07-20 13:46:05 +0500

    Aside texts fixed & added into options

--- resources/views/components/aside-popular-categories.blade.php
+++ resources/views/components/aside-popular-categories.blade.php
@@ -2,7 +2,7 @@
 
 <div class="aside-popular-categories theme-styled-block">
     <h1 class="aside-popular-categories__title main-title">Категорияҳои маъмул</h1>
-    <p class="aside-popular-categories__text">Тут немного текста о том, что здесь категории, которые чаще всего выберают посетители сайта.</p>
+    <div class="aside-popular-categories__text">{!! App\Models\Option::where('key', 'popular-categories-text')->first()->value !!}</div>
     
     <ul class="categories-card-list">
         @foreach ($categories as $category)
